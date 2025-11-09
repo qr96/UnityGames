5a50d6f4f2864f981233db24f5eb7659788650d3
qr96 <qr96@naver.com>
2025-11-09 16:37:02 +0900

    게임 오브젝트 풀 관리자 생성

---
+++ GameClient/Assets/Scripts/InGame/GameObjectManager.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectManager : MonoBehaviour
+{
+    public List<ResourceMeta<GameObject>> metaList = new List<ResourceMeta<GameObject>>();
+
+    Dictionary<string, Queue<GameObject>> poolDic = new Dictionary<string, Queue<GameObject>>();
+    Dictionary<string, ResourceMeta<GameObject>> metaDic = new Dictionary<string, ResourceMeta<GameObject>>();
+
+    readonly string prefabPath = "Prefab/";
+
+    private void Awake()
+    {
+        InitializeMeta();
+    }
+
+    public bool TryCreate<T>(string name, out GameObject item)
+    {
+        item = null;
+
+        if (!poolDic.ContainsKey(name))
+        {
+            Debug.LogError($"Create() No Key in poolDic. name={name}");
+            return false;
+        }
+        else if (!metaDic.ContainsKey(name))
+        {
+            Debug.LogError($"Create() No Key in metaDic. name={name}");
+            return false;
+        }
+
+        // 풀에서 찾고 없으면 생성
+        var go = poolDic[name].Count < metaDic[name].maxSize ? Instantiate(metaDic[name].resource) : poolDic[name].Dequeue();
+        poolDic[name].Enqueue(go);
+        go.SetActive(true);
+
+        // item에 할당 및 true 반환
+        item = go;
+        return true;
+    }
+
+    // 리소스 로드, Pool 및 Meta 딕셔너리 초기화
+    void InitializeMeta()
+    {
+        foreach (var meta in metaList)
+        {
+            var fullPath = prefabPath + meta.name;
+            var prefab = Resources.Load<GameObject>(fullPath);
+
+            meta.resource = prefab;
+            poolDic.Add(meta.name, new Queue<GameObject>());
+            metaDic.Add(meta.name, meta);
+        }
+    }
+}
+
+[System.Serializable]
+public class ResourceMeta<T>
+{
+    public string name;
+    public int maxSize;
+
+    // 이 필드는 로드 후 임시로 프리팹을 저장합니다.
+    [HideInInspector] public T resource;
+}
