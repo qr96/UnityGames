f61b9e01b01bfcc2bf0ccd0cbcea7989765cfddc
qr96 <qr96@naver.com>
2025-12-07 11:36:49 +0900

    볼 쏘는 스크립트 추가

---
+++ GameClient/Assets/Scripts/InGame/BallLauncher.cs
@@ -0,0 +1,106 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BallLauncher : MonoBehaviour
+{
+    public GameObject guideLine;
+
+    public float launchPower = 500f;
+    public float maxDragDistance = 2f;
+
+    private Rigidbody rb;
+    private Vector3 initialPosition; // 슈터 위치 (발사 시작 위치)
+    private bool isDragging = false; // 현재 드래그 중인지 상태
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (!rb.isKinematic)
+        {
+            if (rb.linearVelocity != Vector3.zero)
+                transform.forward = rb.linearVelocity.normalized;
+
+            return;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            HandleMouseDown();
+        }
+
+        if (isDragging)
+        {
+            HandleMouseDrag();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            HandleMouseUp();
+        }
+    }
+
+    private void HandleMouseDown()
+    {
+        // UI 위에 포인터가 있으면 입력을 무시 (UI 가로채기 방지)
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            return;
+        }
+
+        isDragging = true;
+        rb.isKinematic = true; // 만약을 위해 다시 Kinematic 설정
+        initialPosition = transform.position;
+    }
+
+    private void HandleMouseDrag()
+    {
+        Vector3 launchDirection = GetLaunchVector();
+        guideLine.transform.forward = launchDirection;
+    }
+
+    private void HandleMouseUp()
+    {
+        if (!isDragging) return;
+        isDragging = false;
+
+        Vector3 launchDirection = GetLaunchVector();
+        if (launchDirection.magnitude < 0.1f)
+            return;
+
+        rb.isKinematic = false; // 물리 엔진 활성화
+        rb.AddForce(launchDirection.normalized * launchDirection.magnitude * launchPower, ForceMode.Impulse);
+
+        guideLine.gameObject.SetActive(false);
+    }
+
+    Vector3 GetMouseWolrdPos()
+    {
+        Vector3 mouseScreenPos = Input.mousePosition;
+        mouseScreenPos.z = Camera.main.nearClipPlane + 10f;
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mouseScreenPos);
+
+        return mouseWorldPos;
+    }
+
+    Vector3 GetDragVector()
+    {
+        Vector3 dragVector = GetMouseWolrdPos() - initialPosition;
+        dragVector.y = 0f;
+
+        return dragVector;
+    }
+
+    Vector3 GetLaunchVector()
+    {
+        Vector3 launchDirection = GetDragVector();
+
+        if (launchDirection.magnitude > maxDragDistance)
+            launchDirection = launchDirection.normalized * maxDragDistance;
+
+        return launchDirection;
+    }
+}
